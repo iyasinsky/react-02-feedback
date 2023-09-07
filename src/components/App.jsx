84e9ca4c92abb8318ba589b25c833cc87b1c0695
Igor Yasinsky <iyasinskyy@gmail.com>
2023-09-07 18:28:58 +0300

    She is alive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,54 @@
+import { Component } from 'react';
+// import PropTypes from 'prop-types';
 import { GlobalStyle } from 'helpers/GlobalStyle';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
 
-export const App = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <div>React template</div>
-    </>
-  );
-};
+export class App extends Component {
+  static propTypes = {};
+  static defaultProps = {};
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    return Math.round((this.state.good * 100) / this.countTotalFeedback());
+  }
+
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,9 @@
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return options.map(option => {
+    return (
+      <button key={option} onClick={() => onLeaveFeedback(option)}>
+        {option}
+      </button>
+    );
+  });
+};
